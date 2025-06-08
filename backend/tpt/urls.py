7937cc49ee77336166fc7b9aca5b78f8d6cba1c4
César Richard <cesar@crichard.fr>
2025-06-08 13:43:51 +0200

    [FEAT] Ajout d'un endpoint de vérification de santé dans urls.py, mise à jour des probes de santé et des paramètres de déploiement dans les fichiers Kubernetes pour le backend et le frontend, et correction de la référence au certificat TLS dans le gateway Istio.

--- backend/tpt/urls.py
+++ backend/tpt/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
 from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
+
+def health_check(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', health_check, name='health_check'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
