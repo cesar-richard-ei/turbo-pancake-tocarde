36c1087d93b451b0671aa10a19bb968297f84bab
César Richard <cesar@crichard.fr>
2025-06-08 13:15:45 +0200

    [FEAT] Ajout de Whitenoise pour la gestion des fichiers statiques : mise à jour de requirements.txt et configuration du middleware dans settings.py, ainsi que définition du stockage des fichiers statiques compressés.

--- backend/tpt/settings.py
+++ backend/tpt/settings.py
@@ -27,6 +27,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'tpt.urls'
@@ -89,6 +90,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
