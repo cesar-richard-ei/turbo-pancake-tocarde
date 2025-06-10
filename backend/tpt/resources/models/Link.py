e17383c7dbd6328ddda4867f0f4858a30543eb88
César Richard <cesar@crichard.fr>
2025-06-11 00:31:57 +0200

    [STYLE] Ajout de verbose_name et help_text pour les champs du modèle Link afin d'améliorer la lisibilité et la documentation des ressources.

--- backend/tpt/resources/models/Link.py
+++ backend/tpt/resources/models/Link.py
@@ -10,12 +10,31 @@ class Link(models.Model):
     A Link is a link for an external web resource.
     """
 
-    name: str = models.CharField(max_length=255)
-    description: str = models.TextField(null=True, blank=True)
-    url: str = models.URLField(max_length=255)
-    is_active: bool = models.BooleanField(default=True)
-    created_at: datetime = models.DateTimeField(auto_now_add=True)
-    updated_at: datetime = models.DateTimeField(auto_now=True)
+    name: str = models.CharField(
+        max_length=255, verbose_name="Nom", help_text="Nom de la ressource"
+    )
+    description: str = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name="Description",
+        help_text="Description de la ressource",
+    )
+    url: str = models.URLField(
+        max_length=255, verbose_name="URL", help_text="URL de la ressource"
+    )
+    is_active: bool = models.BooleanField(
+        default=True, verbose_name="Actif", help_text="Si la ressource est active"
+    )
+    created_at: datetime = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Date de création",
+        help_text="Date de création de la ressource",
+    )
+    updated_at: datetime = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Date de mise à jour",
+        help_text="Date de mise à jour de la ressource",
+    )
 
     class Meta:
         verbose_name = "Link"
